045662ce68178bcf67f4c509a9bae2c1d47fe72c
DCarr40 <davidwcarruthers@yahoo.com>
2021-12-30 16:38:41 -0500

    added my first python function to determine if its hot cold or warm

---
+++ index.py
@@ -0,0 +1,19 @@
+def test():
+    
+    is_it_hot = False
+   
+    is_it_cold = False
+
+    name = input("Good morning! What's your name?")
+
+    if is_it_hot:
+        print(f"It's hot outside {name}")
+
+    elif is_it_cold:
+        print(f"It's cold outside {name}")
+
+    else:
+        print(f"It's warm outside {name}")
+    
+ 
+test()
