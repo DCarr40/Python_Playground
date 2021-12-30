d75d722c9d526d5d36506f38930a85edd5aea61b
DCarr40 <davidwcarruthers@yahoo.com>
2021-12-30 17:29:37 -0500

    practiced concatenation

--- index.py
+++ index.py
@@ -82,3 +82,22 @@ print(6**6)
 print(7**2)
 print(8**2)
 print(6**4)
+
+#modulo operations
+my_team = 27%4
+print(my_team)
+
+#Concatenation
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+
+message = f"{string1 }{string2 }{string3 }{string4 }{string5 }{string6 }"
+
+
+print(message)
