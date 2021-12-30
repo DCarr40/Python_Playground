319a60bffe7929591cd50d8fcd5a7fac47abc31a
DCarr40 <davidwcarruthers@yahoo.com>
2021-12-30 17:20:54 -0500

    exponent practice

--- index.py
+++ index.py
@@ -68,3 +68,17 @@ quilt_length = 8
 first_quilt = quilt_length * quilt_width
 
 print(first_quilt)
+
+# Calculation of squares for:
+# 6x6 quilt
+
+# 7x7 quilt
+
+# 8x8 quilt
+
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+
+print(6**6)
+print(7**2)
+print(8**2)
+print(6**4)
