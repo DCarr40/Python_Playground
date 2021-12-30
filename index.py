c5b5844e0f3087eff05e87618510c245de0738f9
DCarr40 <davidwcarruthers@yahoo.com>
2021-12-30 16:55:18 -0500

    updated meal to reflect breakfast lunch and dinner

--- index.py
+++ index.py
@@ -26,3 +26,23 @@ test()
 
 my_name = "David C."
 print(f"Hello {my_name}")
+
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "pizza"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "chocolate cake"
+# Printing out dinner
+print("Dinner:")
+print(meal)
